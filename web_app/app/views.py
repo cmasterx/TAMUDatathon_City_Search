d4cc95e58a88927fad95bd51c630f2e85c2d0403
Charlemagne Wong <frian13@hotmail.com>
2020-10-17 21:51:54 -0500

    Changes

--- web_app/app/views.py
+++ web_app/app/views.py
@@ -2,9 +2,19 @@ from flask.helpers import send_from_directory
 from app import app
 from flask import Flask, jsonify, request, render_template, flash, redirect, url_for, send_from_directory
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        print(request.form)
+        return request.form
+    
+    print(request.form)
+    
+    return 'Howdy!'
+
 @app.route('/', methods=['GET'])
 def root():
-    return send_from_directory('./html', 'index.html')
+    return render_template('./html/index.html')
 
 @app.errorhandler(404)
 def handler404(e):
